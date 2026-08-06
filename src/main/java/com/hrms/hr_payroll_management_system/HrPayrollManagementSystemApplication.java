78b40a86b317d58d75d6248ba92b5c29fb960b18
Sang <sengsang2006@gmail.com>
2026-08-06 11:47:07 +0700

    Initial Spring Boot project

---
+++ src/main/java/com/hrms/hr_payroll_management_system/HrPayrollManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hrms.hr_payroll_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayrollManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrms/hr_payroll_management_system/HrPayrollManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.hr_payroll_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
